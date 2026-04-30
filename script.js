143bf2035748ae94491cd88ca0419d4ecd6477fb
Antony <antonynnascimento@hotmail.com>
2026-04-30 20:23:34 -0300

    meu primeiro projeto

---
+++ script.js
@@ -0,0 +1,54 @@
+let lista = document.getElementById("lista");
+
+// carregar ao abrir
+window.onload = function () {
+  let tarefas = JSON.parse(localStorage.getItem("tarefas")) || [];
+
+  tarefas.forEach(t => criarTarefa(t));
+};
+
+function adicionar() {
+  let input = document.getElementById("tarefa");
+
+  if (input.value === "") return;
+
+  criarTarefa(input.value);
+
+  salvar();
+
+  input.value = "";
+}
+
+function criarTarefa(texto) {
+  let item = document.createElement("li");
+
+  let span = document.createElement("span");
+  span.innerText = texto;
+
+  span.onclick = function () {
+    span.style.textDecoration = "line-through";
+    salvar();
+  };
+
+  let botao = document.createElement("button");
+  botao.innerText = "X";
+
+  botao.onclick = function () {
+    item.remove();
+    salvar();
+  };
+
+  item.appendChild(span);
+  item.appendChild(botao);
+  lista.appendChild(item);
+}
+
+function salvar() {
+  let tarefas = [];
+
+  document.querySelectorAll("li span").forEach(span => {
+    tarefas.push(span.innerText);
+  });
+
+  localStorage.setItem("tarefas", JSON.stringify(tarefas));
+}
